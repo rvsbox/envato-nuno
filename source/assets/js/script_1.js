b0dae7bd4f4805a94b5afa3dd70bbba046776570
rvsbox <rvsbox7@gmail.com>
2021-04-09 21:55:40 +0200

    refactoring

--- source/assets/js/script_1.js
+++ source/assets/js/script_1.js
@@ -2,18 +2,18 @@ window.onload = function () {
 
     // START HEADER
     function setWidthHuman(){
-        const nameWidth = document.getElementById('humanName').offsetWidth
-        const specWidth = document.getElementById('humanSpecialty').offsetWidth
+        const nameWidth = document.getElementById('hmnName').offsetWidth
+        const specWidth = document.getElementById('hmnSpec').offsetWidth
 
         console.log(nameWidth)
         console.log(specWidth)
 
         if (nameWidth > specWidth) {
-            document.getElementById('humanSpecialty').style.width = nameWidth + "px"
-            document.getElementById('humanAbout').style.width = nameWidth + "px"
+            document.getElementById('hmnSpec').style.width = nameWidth + "px"
+            document.getElementById('hmnAbout').style.width = nameWidth + "px"
         } else {
-            document.getElementById('humanName').style.width = specWidth + "px"
-            document.getElementById('humanAbout').style.width = specWidth + "px"
+            document.getElementById('hmnName').style.width = specWidth + "px"
+            document.getElementById('hmnAbout').style.width = specWidth + "px"
         }
     }
 

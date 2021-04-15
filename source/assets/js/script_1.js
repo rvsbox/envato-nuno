54f559e7059827357946ff71cb49bf7030addea9
rvsbox <rvsbox7@gmail.com>
2021-04-15 23:36:48 +0200

    refactoring

--- source/assets/js/script_1.js
+++ source/assets/js/script_1.js
@@ -20,57 +20,71 @@ window.onload = function () {
 
 
 
-    function setSizeCircles() {
-        let cirWidth = document.getElementById('cirSb').offsetWidth
-        let cirHeight = document.getElementById('cirSb').offsetHeight
+    // function setSizePosCircles() {
+    //     let cirWidth = document.getElementById('cirSb').offsetWidth
+    //     let cirHeight = document.getElementById('cirSb').offsetHeight
+    //
+    //
+    //
+    //
+    //
+    //     window.addEventListener('resize', event => {
+    //         // let windowWidth = document.body.offsetWidth
+    //         let windowHeight = document.body.offsetHeight
+    //
+    //         // let changeCirWidth = Math.round((920*windowHeight)/1060)
+    //         let changeCirSize = Math.round((920*windowHeight)/1060)
+    //
+    //
+    //         // let cirTop = document.getElementById('cir').offsetTop
+    //
+    //         let changeCirTop = Math.round((windowHeight - )
+    //
+    //         document.getElementById('cir').style.top = changeCirTop + 'px'
+    //
+    //
+    //         console.log(windowHeight)
+    //
+    //         if (windowHeight <= 1060) {
+    //             document.getElementById('cirSb').style.width = changeCirSize + 'px'
+    //             document.getElementById('cirSb').style.height = changeCirSize + 'px'
+    //
+    //
+    //             // console.log(cirTop)
+    //             // let changeCirTop = Math.round((cirTop*windowHeight)/1060)
+    //
+    //
+    //
+    //
+    //             console.log(changeCirTop)
+    //         } else {
+    //             document.getElementById('cirSb').style.width = 920 + 'px'
+    //             document.getElementById('cirSb').style.height = 920 + 'px'
+    //         }
+    //
+    //
+    //         // console.log(changeCirTop)
+    //         // console.log(windowWidth)
+    //         // console.log(windowHeight)
+    //
+    //
+    //     })
+    // }
 
+    function setSizePosCircles() {
+        let bodyW = document.body.offsetWidth
+        let bodyH = document.body.offsetHeight
 
+        let cirW = document.getElementById('cir').offsetWidth
+        let cirH = document.getElementById('cir').offsetHeight
 
+        document.getElementById('cir').style.marginLeft = ((bodyW - cirW)/2) + 'px'
+        document.getElementById('cir').style.marginTop = ((bodyH - cirH)/2) + 'px'
 
+        // console.log(cirW, cirH)
 
-        window.addEventListener('resize', event => {
-            // let windowWidth = document.body.offsetWidth
-            let windowHeight = document.body.offsetHeight
-
-            // let changeCirWidth = Math.round((920*windowHeight)/1060)
-            let changeCirSize = Math.round((920*windowHeight)/1060)
-
-
-            // let cirTop = document.getElementById('cir').offsetTop
-
-            let changeCirTop = Math.round((windowHeight - )
-
-            document.getElementById('cir').style.top = changeCirTop + 'px'
-
-
-            console.log(windowHeight)
-
-            if (windowHeight <= 1060) {
-                document.getElementById('cirSb').style.width = changeCirSize + 'px'
-                document.getElementById('cirSb').style.height = changeCirSize + 'px'
-
-
-                // console.log(cirTop)
-                // let changeCirTop = Math.round((cirTop*windowHeight)/1060)
-
-
-
-
-                console.log(changeCirTop)
-            } else {
-                document.getElementById('cirSb').style.width = 920 + 'px'
-                document.getElementById('cirSb').style.height = 920 + 'px'
-            }
-
-
-            // console.log(changeCirTop)
-            // console.log(windowWidth)
-            // console.log(windowHeight)
-
-
-        })
     }
 
-    setSizeCircles()
+    setSizePosCircles()
 
 }

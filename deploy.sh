97e1e15c106eb1338354994261582b3b80de0b66
rvsbox <rvsbox7@gmail.com>
2022-01-10 17:13:27 +0200

    refactoring

--- deploy.sh
+++ deploy.sh
@@ -10,25 +10,3 @@ ssh rvsbox@185.119.59.186 << EOF
     unzip /home/rvsbox/www/envato-nuno/build.zip -d /home/rvsbox/www/envato-nuno/public_html
     rm -f /home/rvsbox/www/envato-nuno/build.zip
 EOF
-
-
-# rm -f build.zip
-# -f исключить подтверждение 'yes', 'no' и сразу удалить файл
-
-# не включать папку в архив
-# -x "build/alpha-clear/*"
-
-# sudo zip -rq build.zip build;
-# -q - вывод минимум информации о процессе 
-# -r - обозначает рекурсивно
-
-# scp test.txt rvsbox@185.119.59.186:/home/rvsbo
-
-
-# echo 'script is running'
-# ls -l
-
-
-
-# rvsbox@185.119.59.186
-# scp test.txt rvsbox@185.119.59.186:/home/rvsbox

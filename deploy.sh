445bab9fd5447a098fc4ca0b0d78126c2c0b6d35
rvsbox <rvsbox7@gmail.com>
2022-02-07 02:19:52 +0200

    refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd build && sudo zip -r build.zip . -x "alpha-clear/*"
+#cd build && sudo zip -r build.zip . -x "alpha-clear/*"
+cd build/alpha && sudo zip -r build.zip .
 scp build.zip rvsbox@185.119.59.186:/home/rvsbox/www/envato-nuno
 rm -f build.zip
 
 
 ssh rvsbox@185.119.59.186 << EOF
-    rm -r /home/rvsbox/www/envato-nuno/public_html/alpha
+    rm -r /home/rvsbox/www/envato-nuno/public_html
     unzip /home/rvsbox/www/envato-nuno/build.zip -d /home/rvsbox/www/envato-nuno/public_html
     rm -f /home/rvsbox/www/envato-nuno/build.zip
 EOF
